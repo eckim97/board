877f46b4cccbad66631f0a78ad9bdb076fa97393
eunchaan <weoncheol1@naver.com>
2023-10-05 22:36:26 +0900

    feat : 게시판 페이징 기능 구현

---
+++ src/main/java/board/config/DataRestConfig.java
@@ -0,0 +1,25 @@
+package board.config;
+
+import board.domain.ArticleComment;
+import board.domain.Hashtag;
+import board.domain.UserAccount;
+import board.domain.Article;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
+        );
+    }
+
+}
---
+++ src/main/java/board/repository/querydsl/HashtagRepositoryCustomImpl.java
@@ -0,0 +1,24 @@
+package board.repository.querydsl;
+
+import board.domain.Hashtag;
+import board.domain.QHashtag;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class HashtagRepositoryCustomImpl extends QuerydslRepositorySupport implements HashtagRepositoryCustom {
+
+    public HashtagRepositoryCustomImpl() {
+        super(Hashtag.class);
+    }
+
+    @Override
+    public List<String> findAllHashtagNames() {
+        QHashtag hashtag = QHashtag.hashtag;
+
+        return from(hashtag)
+                .select(hashtag.hashtagName)
+                .fetch();
+    }
+
+}
