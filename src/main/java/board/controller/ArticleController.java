780fe72c939af12258e544a9a1d7a6975937f83b
eunchaan <weoncheol1@naver.com>
2023-10-22 21:41:41 +0900

    #15 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영 테스트는 'Page' 클래스를 이용하기 때문에 하는 김에 페이징과 정렬을 함께 테스트 나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/board/controller/ArticleController.java
+++ src/main/java/board/controller/ArticleController.java
@@ -39,10 +39,11 @@ public class ArticleController {
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
         map.addAttribute("articles", articles);
-        map.addAttribute("paginationBarNumbers", barNumbers);
-        map.addAttribute("searchTypes", SearchType.values());
-        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        map.addAttribute("paginationbarNumbers", barNumbers);
+//        map.addAttribute("paginationBarNumbers", barNumbers);
+//        map.addAttribute("searchTypes", SearchType.values());
+//        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
+//        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
 
         return "articles/index";
     }
@@ -53,76 +54,76 @@ public class ArticleController {
 
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
-        map.addAttribute("totalCount", articleService.getArticleCount());
-        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
+//        map.addAttribute("totalCount", articleService.getArticleCount());
+//        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
 
         return "articles/detail";
     }
 
-    @GetMapping("/search-hashtag")
-    public String searchArticleHashtag(
-            @RequestParam(required = false) String searchValue,
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map
-    ) {
-        Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
-        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
-        List<String> hashtags = articleService.getHashtags();
-
-        map.addAttribute("articles", articles);
-        map.addAttribute("hashtags", hashtags);
-        map.addAttribute("paginationBarNumbers", barNumbers);
-        map.addAttribute("searchType", SearchType.HASHTAG);
-
-        return "articles/search-hashtag";
-    }
-
-    @GetMapping("/form")
-    public String articleForm(ModelMap map) {
-        map.addAttribute("formStatus", FormStatus.CREATE);
-
-        return "articles/form";
-    }
-
-    @PostMapping("/form")
-    public String postNewArticle(
-            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
-            ArticleRequest articleRequest
-    ) {
-        articleService.saveArticle(articleRequest.toDto(boardPrincipal.toDto()));
-
-        return "redirect:/articles";
-    }
-
-    @GetMapping("/{articleId}/form")
-    public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
-        ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
-
-        map.addAttribute("article", article);
-        map.addAttribute("formStatus", FormStatus.UPDATE);
-
-        return "articles/form";
-    }
-
-    @PostMapping("/{articleId}/form")
-    public String updateArticle(
-            @PathVariable Long articleId,
-            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
-            ArticleRequest articleRequest
-    ) {
-        articleService.updateArticle(articleId, articleRequest.toDto(boardPrincipal.toDto()));
-
-        return "redirect:/articles/" + articleId;
-    }
-
-    @PostMapping("/{articleId}/delete")
-    public String deleteArticle(
-            @PathVariable Long articleId,
-            @AuthenticationPrincipal BoardPrincipal boardPrincipal
-    ) {
-        articleService.deleteArticle(articleId, boardPrincipal.getUsername());
-
-        return "redirect:/articles";
-    }
+//    @GetMapping("/search-hashtag")
+//    public String searchArticleHashtag(
+//            @RequestParam(required = false) String searchValue,
+//            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+//            ModelMap map
+//    ) {
+//        Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
+//        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+//        List<String> hashtags = articleService.getHashtags();
+//
+//        map.addAttribute("articles", articles);
+//        map.addAttribute("hashtags", hashtags);
+//        map.addAttribute("paginationBarNumbers", barNumbers);
+//        map.addAttribute("searchType", SearchType.HASHTAG);
+//
+//        return "articles/search-hashtag";
+//    }
+//
+//    @GetMapping("/form")
+//    public String articleForm(ModelMap map) {
+//        map.addAttribute("formStatus", FormStatus.CREATE);
+//
+//        return "articles/form";
+//    }
+//
+//    @PostMapping("/form")
+//    public String postNewArticle(
+//            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+//            ArticleRequest articleRequest
+//    ) {
+//        articleService.saveArticle(articleRequest.toDto(boardPrincipal.toDto()));
+//
+//        return "redirect:/articles";
+//    }
+//
+//    @GetMapping("/{articleId}/form")
+//    public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
+//        ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
+//
+//        map.addAttribute("article", article);
+//        map.addAttribute("formStatus", FormStatus.UPDATE);
+//
+//        return "articles/form";
+//    }
+//
+//    @PostMapping("/{articleId}/form")
+//    public String updateArticle(
+//            @PathVariable Long articleId,
+//            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+//            ArticleRequest articleRequest
+//    ) {
+//        articleService.updateArticle(articleId, articleRequest.toDto(boardPrincipal.toDto()));
+//
+//        return "redirect:/articles/" + articleId;
+//    }
+//
+//    @PostMapping("/{articleId}/delete")
+//    public String deleteArticle(
+//            @PathVariable Long articleId,
+//            @AuthenticationPrincipal BoardPrincipal boardPrincipal
+//    ) {
+//        articleService.deleteArticle(articleId, boardPrincipal.getUsername());
+//
+//        return "redirect:/articles";
+//    }
 
 }
